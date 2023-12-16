f5119cf70aced221367f8a85774043b81f2f7b0b
faisal-work <faisal@rently.com>
2023-12-16 19:48:31 +0530

    moidified module export logic

--- greet.js
+++ greet.js
@@ -1,18 +1,13 @@
-var MyModule = function(message) {
-    var template = '<h2>Hi there ' + message + '!</h2>';
+var MyModule = function (message) {
+  var template = "<h2>Hi there " + message + "!</h2>";
 
-    return template;
+  return template;
+};
+
+if (typeof module !== "undefined" && module.exports) {
+  // If in a Node.js environment, export the module
+  module.exports = MyModule;
+} else {
+  // If in a browser environment, attach the module to the global object
+  (typeof self !== "undefined" ? self : window).MyModule = MyModule;
 }
-
-
-// module.exports = MyModule;   
-
-
-if (typeof module !== 'undefined' && module.exports) {
-    // If in a Node.js environment, export the module
-    module.exports = MyModule;
-  } else {
-    // If in a browser environment, attach the module to the global object (self or window)
-    (typeof self !== 'undefined' ? self : window).MyModule = MyModule;
-  }
-
--- src/index.js
+++ src/index.js
@@ -1,40 +1,8 @@
-// window.addEventListener('load', function () {
-//     var greet = require('../greet');
-//     var greeting = greet('Wecome to React gatherings!');
-//     var body = document.getElementsByTagName('body')[0];
-//     body.innerHTML = greeting;
-// });
+window.addEventListener("load", function () {
+  // Check if the global object has MyModule
+  var MyModule = window.MyModule;
+  var greeting = MyModule("Welcome to React gatherings!");
 
-
-
-
-window.addEventListener('load', function () {
-    // Check if the global object has MyModule
-    var MyModule = (typeof window !== 'undefined' && window.MyModule) ? window.MyModule : null;
-
-    // If MyModule is not available, dynamically load greet.js
-    if (!MyModule) {
-        var script = document.createElement('script');
-        script.src = '../greet.js';
-
-        script.onload = function () {
-            MyModule = window.MyModule;
-            // Use the imported module
-            var greeting = MyModule('Welcome to React gatherings!');
-            
-            var body = document.getElementsByTagName('body')[0];
-            body.innerHTML = greeting;
-        };
-
-        document.head.appendChild(script);
-    } else {
-        // Use the imported module
-        var greeting = MyModule('Welcome to React gatherings!');
-        
-        var body = document.getElementsByTagName('body')[0];
-        body.innerHTML = greeting;
-    }
+  var body = document.getElementsByTagName("body")[0];
+  body.innerHTML = greeting;
 });
-
-
-//run npx webpack 
