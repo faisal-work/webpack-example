89ee8e966dc2330077921d79b3bf288c42cbf9f6
faisal-work <faisal@rently.com>
2023-12-16 19:55:34 +0530

    added webpack bundling

--- greet.js
+++ greet.js
@@ -5,14 +5,5 @@ var MyModule = function(message) {
 }
 
 
-// module.exports = MyModule;   
-
-
-if (typeof module !== 'undefined' && module.exports) {
-    // If in a Node.js environment, export the module
-    module.exports = MyModule;
-  } else {
-    // If in a browser environment, attach the module to the global object (self or window)
-    (typeof self !== 'undefined' ? self : window).MyModule = MyModule;
-  }
+module.exports = MyModule;   
 
