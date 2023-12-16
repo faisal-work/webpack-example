40c430d7cb320bc1df195ee617fea545ac3bb45d
faisal-work <faisal@rently.com>
2023-12-16 19:23:09 +0530

    show failure of commonjs module import

--- greet.js
+++ greet.js
@@ -5,14 +5,14 @@ var MyModule = function(message) {
 }
 
 
-// module.exports = MyModule;   
+module.exports = MyModule;   
 
 
-if (typeof module !== 'undefined' && module.exports) {
-    // If in a Node.js environment, export the module
-    module.exports = MyModule;
-  } else {
-    // If in a browser environment, attach the module to the global object (self or window)
-    (typeof self !== 'undefined' ? self : window).MyModule = MyModule;
-  }
+// if (typeof module !== 'undefined' && module.exports) {
+//     // If in a Node.js environment, export the module
+//     module.exports = MyModule;
+//   } else {
+//     // If in a browser environment, attach the module to the global object (self or window)
+//     (typeof self !== 'undefined' ? self : window).MyModule = MyModule;
+//   }
 
